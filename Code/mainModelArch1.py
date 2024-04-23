cd312a44d904e7fc7b0b67e3ae404be05ead7a8b
ayanvishwakarma <102685152+ayanvishwakarma@users.noreply.github.com>
2024-04-24 05:02:39 +0530

    Update mainModelArch1.py

--- Code/mainModelArch1.py
+++ Code/mainModelArch1.py
@@ -216,11 +216,11 @@ if __name__ == '__main__':
         val_task1_F1.append(val_metrics['Task1-Macro-F1'])
         val_task2_F1.append(val_metrics['Task2-F1'])
         
-        print(f"Train Loss: {train_epoch_loss[e]:20.6f} \t Val Loss: {val_epoch_loss[e]:20.6f}")
-        print(f"Train Task1-Macro-F1: {train_metrics['Task1-Macro-F1']:20.6f} \t Val Task1-Macro-F1: {val_metrics['Task1-Macro-F1']:20.6f}")
-        print(f"Train Task2-F1: {train_metrics['Task2-F1']:20.6f} \t Val Task2-F1: {val_metrics['Task2-F1']:20.6f}")
-        print(f"Train Task1-Entailment-F1: {train_metrics['Task1-Entailment-F1']:20.6f} \t Val Task1-Entailment-F1: {val_metrics['Task1-Entailment-F1']:20.6f}")
-        print(f"Train Task1-Contradiction-F1: {train_metrics['Task1-Contradiction-F1']:20.6f} \t Val Task1-Contradiction-F1: {val_metrics['Task1-Contradiction-F1']:20.6f}")
+        print("{:>50}".format(f"Train Loss: {train_epoch_loss[e]:20.6f}"), "{:>50}".format("Val Loss: {val_epoch_loss[e]:20.6f}"))
+        print("{:>50}".format(f"Train Task1-Macro-F1: {train_metrics['Task1-Macro-F1']:20.6f}"), "{:>50}".format("Val Task1-Macro-F1: {val_metrics['Task1-Macro-F1']:20.6f}"))
+        print("{:>50}".format(f"Train Task2-F1: {train_metrics['Task2-F1']:20.6f}"), "{:>50}".format("Val Task2-F1: {val_metrics['Task2-F1']:20.6f}"))
+        print("{:>50}".format(f"Train Task1-Entailment-F1: {train_metrics['Task1-Entailment-F1']:20.6f}"), "{:>50}".format("Val Task1-Entailment-F1: {val_metrics['Task1-Entailment-F1']:20.6f}"))
+        print("{:>50}".format(f"Train Task1-Contradiction-F1: {train_metrics['Task1-Contradiction-F1']:20.6f}"), "{:>50}".format("Val Task1-Contradiction-F1: {val_metrics['Task1-Contradiction-F1']:20.6f}"))
 
         # early stopping
         early_stopping(val_metrics['Task1-Macro-F1'], model)
