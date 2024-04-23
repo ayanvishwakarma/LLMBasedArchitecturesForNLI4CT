263db977d240fc56527e8cbde9fa9a87ffb121e5
ayanvishwakarma <102685152+ayanvishwakarma@users.noreply.github.com>
2024-04-24 04:41:17 +0530

    Update utils.py

--- Code/utils.py
+++ Code/utils.py
@@ -9,9 +9,9 @@ class EarlyStopping:
     def __init__(self, patience, verbose=False, delta=0, save_path='checkpoint.pt'):
         """
         Args:
-            patience (int): How long to wait after last time AUPRC improved.
+            patience (int): How long to wait after last time score improved.
                             Default: 7
-            verbose (bool): If True, prints a message for each auprc improvement.
+            verbose (bool): If True, prints a message for each score improvement.
                             Default: False
             delta (float): Minimum change in the monitored quantity to qualify as an improvement.
                             Default: 0
@@ -30,7 +30,7 @@ class EarlyStopping:
         if self.best_score is None:
             self.best_score = score
             self.save_checkpoint(score, model)
-        elif auprc < self.best_score + self.delta:
+        elif score < self.best_score + self.delta:
             self.counter += 1
             print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
