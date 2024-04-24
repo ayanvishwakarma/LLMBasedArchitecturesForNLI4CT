34f821ad69e6d06660022c9b12905a221c9a438a
ayanvishwakarma <102685152+ayanvishwakarma@users.noreply.github.com>
2024-04-24 11:41:25 +0530

    Update mainModelArch1.py

--- Code/mainModelArch1.py
+++ Code/mainModelArch1.py
@@ -167,7 +167,9 @@ if __name__ == '__main__':
                 entailment_prob, evidence_prob, entailment_pred, evidence_pred = model.forward(sample)
                 loss = (1 / args.batch_size) * loss_fn(entailment_prob, torch.tensor(sample['label_task1']).to(device), 
                                                        evidence_prob, torch.tensor(sample['label_task2']).to(device))
+                print("abcd")
                 loss.backward()
+                print("efgh")
             
             batch_processed = (batch_processed + 1) % args.batch_size
             if batch_processed == 0:
