e8df9975f376d262d271197f44b0ae4d934cd06d
ayanvishwakarma <102685152+ayanvishwakarma@users.noreply.github.com>
2024-05-04 21:59:42 +0530

    Update evaluate.py

--- Code/evaluate.py
+++ Code/evaluate.py
@@ -3,7 +3,7 @@ from torchmetrics.classification import BinaryCalibrationError
 
 import numpy as np
 import os
-from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.metrics import f1_score, precision_score, recall_score, average_precision_score, roc_auc_score
 
 def task1_metrics(targets, predictions, uuids, args):
     assert all([(uuid in targets and uuid in predictions) for uuid in uuids])
@@ -24,6 +24,9 @@ def task1_metrics(targets, predictions, uuids, args):
     metrics_dict['Task1-Contradiction-F1'] = f1_score(true_contra, pred_contra)
     metrics_dict['Task1-Macro-F1'] = f1_score(true_entail, pred_entail, average='macro')
 
+    metrics_dict['Task1-AUROC'] = roc_auc_score(true_entail, pred_entail)
+    metrics_dict['Task1-AUPRC'] = average_precision_score(true_entail, pred_entail)
+
     bce_score = BinaryCalibrationError(n_bins=args.n_bins, norm='l1')
     metrics_dict['Task1-Calibration'] = float(bce_score(torch.tensor(prob_entail), torch.tensor(true_entail)))
     return metrics_dict
@@ -32,19 +35,26 @@ def task2_metrics(targets, predictions, uuids, args):
     TP = 0.0
     FP = 0.0
     FN = 0.0
-  
+
+    logits = []
+    labels = []
+    
     for uuid in uuids:
         true_evidence_inds = set(targets[uuid]['Primary_evidence_index'])
         predicted_evidence_inds = set(predictions[uuid]['Primary_evidence_index'])
         TP += sum([ind in true_evidence_inds for ind in predictions[uuid]['Primary_evidence_index']])
         FP += sum([ind not in true_evidence_inds for ind in predictions[uuid]['Primary_evidence_index']])
         FN += sum([ind not in predicted_evidence_inds for ind in targets[uuid]['Primary_evidence_index']])
+        logits.extend(predictions[uuid]['Primary_evidence_prob'])
+        labels.extend([1 if x in targets[uuid]['Primary_evidence_index'] else 0 for x in range(len(predictions[uuid]['Primary_evidence_prob']))])
         if targets[uuid]['Type'] == 'Comparison':
             true_evidence_inds = set(targets[uuid]['Secondary_evidence_index'])
             predicted_evidence_inds = set(predictions[uuid]['Secondary_evidence_index'])
             TP += sum([ind in true_evidence_inds for ind in predictions[uuid]['Secondary_evidence_index']])
             FP += sum([ind not in true_evidence_inds for ind in predictions[uuid]['Secondary_evidence_index']])
             FN += sum([ind not in predicted_evidence_inds for ind in targets[uuid]['Secondary_evidence_index']])
+            logits.extend(predictions[uuid]['Secondary_evidence_prob'])
+            labels.extend([1 if x in targets[uuid]['Secondary_evidence_index'] else 0 for x in range(len(predictions[uuid]['Secondary_evidence_prob']))])
     p_score = TP / (TP + FP + 1e-8)
     r_score = TP / (TP + FN + 1e-8)
     F1 = 2 * p_score * r_score / (p_score + r_score + 1e-8)
@@ -53,6 +63,10 @@ def task2_metrics(targets, predictions, uuids, args):
     metrics_dict['Task2-precision'] = p_score
     metrics_dict['Task2-recall'] = r_score
     metrics_dict['Task2-F1'] = F1 
+
+    metrics_dict['Task2-AUROC'] = roc_auc_score(labels, logits)
+    metrics_dict['Task2-AUPRC'] = average_precision_score(labels, logits)
+    
     return metrics_dict
 
 def task1_perturbed_metrics(targets, predictions, uuids, args):
