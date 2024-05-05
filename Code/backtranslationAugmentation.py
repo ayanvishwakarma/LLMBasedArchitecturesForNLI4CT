d5c8003c7fcf819fe14c0e326a10fb998d973ebc
ayanvishwakarma <102685152+ayanvishwakarma@users.noreply.github.com>
2024-05-05 18:07:23 +0530

    Update backtranslationAugmentation.py

--- Code/backtranslationAugmentation.py
+++ Code/backtranslationAugmentation.py
@@ -26,13 +26,20 @@ class BackTranslator:
   
     def __call__(self, texts):
         with torch.no_grad():
-            texts = ['>>fr<< ' + text for text in texts]
-            en_to_fr_inputs = {key: value.to(self.device) for key, value in self.en_to_fr_tokenizer.batch_encode_plus(texts, return_tensors='pt', padding=True).items()}
-            pretexts = [self.en_to_fr_tokenizer.decode(text, skip_special_tokens=True) for text in self.en_to_fr_model.generate(**en_to_fr_inputs)]
-            texts = ['>>en<< ' + text for text in pretexts]
-            fr_to_en_inputs = {key: value.to(self.device) for key, value in self.fr_to_en_tokenizer.batch_encode_plus(texts, return_tensors='pt', padding=True).items()}
-            texts = [self.fr_to_en_tokenizer.decode(text, skip_special_tokens=True) for text in self.fr_to_en_model.generate(**fr_to_en_inputs)]
-            return texts
+            complete_texts = texts
+            backtranslated_texts = []
+            for i in range(0, len(texts), 128):
+                texts = complete_texts[i: i+128]
+                texts = ['>>fr<< ' + text for text in texts]
+                en_to_fr_inputs = {key: value.to(self.device) for key, value in self.en_to_fr_tokenizer.batch_encode_plus(texts, return_tensors='pt', padding=True).items()}
+                pretexts = [self.en_to_fr_tokenizer.decode(text, skip_special_tokens=True) for text in self.en_to_fr_model.generate(**en_to_fr_inputs)]
+
+                texts = ['>>en<< ' + text for text in pretexts]
+                fr_to_en_inputs = {key: value.to(self.device) for key, value in self.fr_to_en_tokenizer.batch_encode_plus(texts, return_tensors='pt', padding=True).items()}
+                texts = [self.fr_to_en_tokenizer.decode(text, skip_special_tokens=True) for text in self.fr_to_en_model.generate(**fr_to_en_inputs)]
+
+                backtranslated_texts.extend(texts)
+            return backtranslated_texts
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
